f3ba5483cde92396ea9015ecbdb65de5b4f4f2b8
Tom Knox <tokn@users.noreply.github.com>
2022-06-21 01:06:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1, maximum-scale=1";
+document.getElementsByTagName('head')[0].appendChild(meta);
+
+
 data = [];
 dataLimitMax = 20;
 
